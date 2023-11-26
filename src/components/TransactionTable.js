81ec08935a86ebac5082ac3c86a26ce001efd7da
+ <+>
2023-11-26 03:20:16 +0300

    trasactions table component

---
+++ src/components/TransactionTable.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function TransactionTable({ transactions }) {
+  return (
+    //table to display the tranasactions
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction) => (
+          <tr key={transaction.id}>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>{transaction.category}</td>
+            <td>{transaction.amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionTable;
